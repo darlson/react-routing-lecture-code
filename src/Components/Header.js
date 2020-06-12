0acbbc9c50dc03e8bc28af36f3af47d304e2cf3c
darlson <davertiser@gmail.com>
2020-06-12 13:59:55 -0600

    updated routes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default props => (
     <div className='header-container'>
         <h1>React Routing</h1>
         <nav>
-            <span>Home</span>
-            <span>About</span>
-            <span>Contact</span>
+            <Link className='nav-links' to='/'>Home</Link>
+            <Link className='nav-links' to='/about'>About</Link>
+            <Link className='nav-links' to='/contact'>Contact</Link>
         </nav>
     </div>
 )
